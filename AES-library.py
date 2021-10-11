19d3f71efdc99d80f9bc789bb8f8e23b9e1e720b
rozakcloud <61219666+rozakcloud@users.noreply.github.com>
2021-10-12 03:06:10 +0700

    add encrypt and decrypt running time

--- AES-library.py
+++ AES-library.py
@@ -5,6 +5,8 @@ import os.path
 from os import listdir
 from os.path import isfile, join
 import time
+import datetime
+import timeit
 
 
 class AesEncrypt:
@@ -21,12 +23,14 @@ class AesEncrypt:
         return iv + cipher.encrypt(message)
 
     def encrypt_file(self, file_name):
+        begin_time = datetime.datetime.now()
         with open(file_name, 'rb') as fo:
             plaintext = fo.read()
         enc = self.encrypt(plaintext, self.key)
         with open(file_name + ".enc", 'wb') as fo:
             fo.write(enc)
         os.remove(file_name)
+        print(datetime.datetime.now() - begin_time)
 
     def decrypt(self, ciphertext, key):
         iv = ciphertext[:AES.block_size]
@@ -35,24 +39,26 @@ class AesEncrypt:
         return plaintext.rstrip(b"\0")
 
     def decrypt_file(self, file_name):
+        begin_time = datetime.datetime.now()
         with open(file_name, 'rb') as fo:
             ciphertext = fo.read()
         dec = self.decrypt(ciphertext, self.key)
         with open(file_name[:-4], 'wb') as fo:
             fo.write(dec)
         os.remove(file_name)
+        print(datetime.datetime.now() - begin_time)
 
 key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
 enc = AesEncrypt(key)
 clear = lambda: os.system('cls')
 
 while True:
-    clear()
+    # clear()
     choice = int(input(
         "1. Ketik '1' untuk encrypt file.\n2. Ketik '2' untuk decrypt file.\n"))
-    clear()
     if choice == 1:
         enc.encrypt_file(str(input("Masukkan nama file untuk di encrypt: ")))
+        
     elif choice == 2:
         enc.decrypt_file(str(input("Masukkan nama file untuk di decrypt: ")))
     elif choice == "exit":
